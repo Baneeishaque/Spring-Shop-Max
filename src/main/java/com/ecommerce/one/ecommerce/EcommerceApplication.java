0078af388879ee83b06bb21fa2dd0ae04baffefe
subedi bibek <bibs2us@outlook.com>
2020-05-23 22:39:40 +0800

    controller and service

--- src/main/java/com/ecommerce/one/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/one/ecommerce/EcommerceApplication.java
@@ -1,9 +1,12 @@
 package com.ecommerce.one.ecommerce;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ecommerce.one.ecommerce.mapper")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
