da87bae3351e99798c13548b808d0d9c15efa971
romi <lama.romi08@gmail.com>
2020-05-25 23:09:37 +0800

    Modifications

--- src/main/java/com/ecommerce/one/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/one/ecommerce/controller/HomeController.java
@@ -10,4 +10,38 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("shop")
+    public String shop() {
+        return "shop";
+    }
+
+    @RequestMapping("about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("contact")
+    public String contact() { return "contact"; }
+
+    @RequestMapping("cart")
+    public String cart() { return "cart"; }
+
+    @RequestMapping("login")
+    public String login() { return "login"; }
+
+    @RequestMapping("adminLogin")
+    public String adminLogin() {return "adminLogin";}
+
+    @RequestMapping("checkout")
+    public String checkout() {return "checkout";}
+
+    @RequestMapping("shopSingle")
+    public String shopSingle() {return "shopSingle";}
+
+    @RequestMapping("signup")
+    public String signup() {return "signup";}
+
+    @RequestMapping("thankYou")
+    public String thankYou() {return "thankYou";}
 }
